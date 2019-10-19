697422541c31f5d77e7c120ef04ec4c643652827
Antonio-Claudiu Purel <antonioclaudiutol@gmail.com>
2019-10-19 16:09:16 +0300

    Added crafting

---
+++ Assets/Scripts/CraftSpear.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftSpear : MonoBehaviour
+{
+    public void CraftSpearAction()
+    {
+        if(Resources.Berry > 0 && Resources.Branch > 0)
+        {
+            Resources.Berry--;
+            Resources.Branch--;
+            Resources.Rock++;
+            Resources.isRockUnlocked = true;
+        }
+    }
+}
