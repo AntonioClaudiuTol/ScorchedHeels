38994ddbf3b57cc87d1481be241b2226cc4cabec
AntonioClaudiuTol <antonioclaudiutol@gmail.com>
2019-11-07 22:31:28 +0200

    Implemented explore button

--- Assets/Scripts/New Scripts/Exploration.cs
+++ Assets/Scripts/New Scripts/Exploration.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Exploration : MonoBehaviour
 {
+    [SerializeField] private GameObject map;
     [SerializeField] private RectTransform mapMask;
     private int progress = 0;
+
+    public void ToggleMap()
+    {
+        map.SetActive(!map.activeSelf);
+    }
     
     public void Explore()
     {
         progress += 3;
-        mapMask.sizeDelta = new Vector2(progress * 100, progress * 100);
+        mapMask.sizeDelta = new Vector2(progress * 25, progress * 25);
         if (progress > 10)
         {
             progress = 0;
